286fd203f52a18ef73893cbbced31a8c69801131
Vig2001 <guruvignesh2001@gmail.com>
2024-03-09 20:57:15 +0000

    Exploration 09/03

--- power_trial.R
+++ power_trial.R
@@ -1,16 +1,16 @@
-n <- 10# change this
+n <- 100# change this
 t_effect <- 2 # change this
 n_sim <- 1000
 pvals <- rep(0, times = n_sim)
 treat_coef <- rep(0, times = n_sim)
 
 nsaa_baseline <- rnorm(2*n,
-                       mean = 15,
-                       sd = 4)
+                       mean = 17.3,
+                       sd = 4.4)
 treatment <- rep(c(0,1), each = n)
 
-nsaa_final <- t_effect*treatment + 0.8*nsaa_baseline + rnorm(2*n,
-                                                           sd = 4)
+nsaa_final <- t_effect*treatment + 3.8*nsaa_baseline + rnorm(2*n,
+                                                           sd = 5.6)
 plot(nsaa_baseline, nsaa_final, col=1+treatment)
 cor(nsaa_baseline, nsaa_final)
 
@@ -20,7 +20,7 @@ for(i in 1:n_sim){
                          sd = 4)
   treatment <- rep(c(0,1), each = n)
   
-  nsaa_final <- t_effect*treatment + 0.8*nsaa_baseline + rnorm(2*n,
+  nsaa_final <- t_effect*treatment - 0.8*nsaa_baseline + rnorm(2*n,
                                                         sd = 4)
   
   df <- data.frame(nsaa_baseline = nsaa_baseline,
